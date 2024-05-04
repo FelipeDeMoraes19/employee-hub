4ac755173aed63208b7d5a03f5cebae508e6a618
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-04 18:07:38 -0300

    Banner

---
+++ organo/src/componentes/Banner/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner(){
+    // JSX
+    return (
+        <header className = "banner">
+            <img src = "/imagens/banner.png" alt = "Banner principal da p[ágina do Organo"/>
+        </header>
+    )
+}
+
+export default Banner
