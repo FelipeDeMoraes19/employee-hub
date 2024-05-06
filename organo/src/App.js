02362b631b84e22dafd755f4b1faa6f4f6912fb7
FelipeMoraes18 <111572342+FelipeMoraes18@users.noreply.github.com>
2024-05-06 13:52:17 -0300

    Mapping collaborators

--- organo/src/App.js
+++ organo/src/App.js
@@ -57,7 +57,13 @@ function App() {
       <Banner />
       <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
 
-      {times.map(time => <Time key={time.nome} nome= {time.nome} corPrimaria={time.corPrimaria} corSecundaria= {time.corSegundaria} />)}
+      {times.map(time => <Time 
+      key={time.nome} 
+      nome= {time.nome} 
+      corPrimaria={time.corPrimaria} 
+      corSecundaria= {time.corSegundaria} 
+      colaboradores={colaboradores.filter(colaborador => colaborador.time == time.nome)}
+      />)}
     </div>
   );
 }
--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,13 +1,13 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({nome, imagem, cargo}) => {
     return(<div className='colaborador'>
         <div className='cabecalho'>
-            <img src = 'https://github.com/FelipeDeMoraes19.png' alt = 'Felipe de Moraes' />
+            <img src={imagem} alt={nome}/>
         </div>
         <div className='rodape'>
-            <h4>Felipe de Moraes</h4>
-            <h5>Aluno </h5>
+            <h4> {nome} </h4>
+            <h5> {cargo} </h5>
         </div>
     </div>)
 }
